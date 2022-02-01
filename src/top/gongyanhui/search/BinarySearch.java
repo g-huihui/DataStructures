a5a2eeee19652b1ea91d0660e577d4c817aa4b10
Taurus <1433723627@qq.com>
2022-02-01 17:35:04 +0800

    [feat]()快速排序

---
+++ src/top/gongyanhui/search/BinarySearch.java
@@ -0,0 +1,10 @@
+package top.gongyanhui.search;
+
+/**
+ * @author Gong Yanhui
+ * @description TODO
+ * @date 2022-02-01 17:21
+ * https://www.cnblogs.com/kyoner/p/11080078.html
+ */
+public class BinarySearch {
+}
