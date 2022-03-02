df6affd8e52ae62b49bbfd5fd0356d04cf429e73
Taurus <1433723627@qq.com>
2022-03-02 17:58:46 +0800

    [feat]():盛最多水的容器

---
+++ src/top/gongyanhui/leetcode/M11.java
@@ -0,0 +1,24 @@
+package top.gongyanhui.leetcode;
+
+/**
+ * @author Gong Yanhui
+ * @description TODO 11. 盛最多水的容器
+ * @date 2022-03-02 15:44:28
+ */
+public class M11 {
+
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            max = Math.max(max, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
