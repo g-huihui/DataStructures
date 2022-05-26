1a5f086a58b1a68b3acc4b379e461206f571a2fb
Taurus <1433723627@qq.com>
2022-05-26 11:46:50 +0800

    [fix]():括号生成

--- src/top/gongyanhui/leetcode/M22.java
+++ src/top/gongyanhui/leetcode/M22.java
@@ -21,17 +21,23 @@ public class M22 {
         return ans;
     }
 
+    /**
+     *
+     * @param open  左括号
+     * @param close 右括号
+     * @param max   括号对数
+     */
     public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
         if (cur.length() == max * 2) {
             ans.add(cur.toString());
             return;
         }
-        if (open < max) {
+        if (open < max) {   //如果左括号没有超过最大括号数
             cur.append('(');
             backtrack(ans, cur, open + 1, close, max);
             cur.deleteCharAt(cur.length() - 1);
         }
-        if (close < open) {
+        if (close < open) { //如果右括号小于左括号数
             cur.append(')');
             backtrack(ans, cur, open, close + 1, max);
             cur.deleteCharAt(cur.length() - 1);
